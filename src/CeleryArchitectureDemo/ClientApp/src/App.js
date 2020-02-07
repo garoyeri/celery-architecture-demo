61e255353732838426149ce9639479ce7ec8ac35
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2020-02-06 21:14:13 -0600

    Reformat JS files using Prettier

--- src/CeleryArchitectureDemo/ClientApp/src/App.js
+++ src/CeleryArchitectureDemo/ClientApp/src/App.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './features/home/Home';
-import { Weather } from './features/weather/Weather';
-import { Counter } from './features/counter/Counter';
-import { Todo } from './features/todo/Todo';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./features/home/Home";
+import { Weather } from "./features/weather/Weather";
+import { Counter } from "./features/counter/Counter";
+import { Todo } from "./features/todo/Todo";
 
 export default class App extends Component {
-    static displayName = App.name;
+  static displayName = App.name;
 
-    render () {
-        return (
-                  <
-            Layout >
-             <
-            Route
-        exact
-        path = '/'
-        component = { Home } /  >  < Route
-        path = '/counter'
-        component = { Counter } /  >  < Route
-        path = '/weather'
-        component = { Weather } /  >  < Route
-        path = '/todo'
-        component = { Todo } /  >  < /
-        Layout > 
+  render() {
+    return (
+      <Layout>
+        <Route exact path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/weather" component={Weather} />
+        <Route path="/todo" component={Todo} />
+      </Layout>
     );
-}
+  }
 }
--- src/CeleryArchitectureDemo/ClientApp/src/features/todo/Todo.js
+++ src/CeleryArchitectureDemo/ClientApp/src/features/todo/Todo.js
@@ -3,72 +3,52 @@ import { TodoList } from "./components/TodoList";
 import { AddItem } from "./components/AddItem";
 
 export class Todo extends Component {
-    static displayName = Todo.name;
+  static displayName = Todo.name;
 
-    constructor(props) {
-        super(props);
-        this.state = { todoItems: [], loading: true };
+  constructor(props) {
+    super(props);
+    this.state = { todoItems: [], loading: true };
 
-        this.reload = this.reload.bind(this);
+    this.reload = this.reload.bind(this);
 
-        this.reload();
-    }
+    this.reload();
+  }
 
-    reload() {
-        fetch("/api/Todo")
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ todoItems: data.todoItems, loading: false });
-            });
-    }
+  reload() {
+    fetch("/api/Todo")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ todoItems: data.todoItems, loading: false });
+      });
+  }
 
-    static renderComponents(todoItems, self) {
-        return (
-        <>
-        <
-        TodoList
-        todoItems = { todoItems }
-        onRequireReload = { self.reload }></
-        TodoList >  < AddItem
-        onRequireReload = { self.reload }></
-        AddItem >  < />
+  static renderComponents(todoItems, self) {
+    return (
+      <>
+        <TodoList
+          todoItems={todoItems}
+          onRequireReload={self.reload}
+        ></TodoList>
+        <AddItem onRequireReload={self.reload}></AddItem>
+      </>
     );
-}
+  }
 
-render()
-{
-    let contents = this.state.loading
-        ? (
-                    <
-        p >
-         <
-        em >
-        Loading...</
-    em >  < /
-    p > 
-) :
-(
-    Todo.renderComponents(this.state.todoItems, this)
-);
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      Todo.renderComponents(this.state.todoItems, this)
+    );
 
-return (
-    <
-    div >
-    <
-    h1 >
-    Todo
-List < /
-h1 >  < p > Keep
-track
-of
-what
-you
-'re doing, really simply.</p>
-{
-    contents
-}
-</
-div > 
-);
-}
+    return (
+      <div>
+        <h1>Todo List </h1>
+        <p> Keep track of what you 're doing, really simply.</p>
+        {contents}
+      </div>
+    );
+  }
 }
