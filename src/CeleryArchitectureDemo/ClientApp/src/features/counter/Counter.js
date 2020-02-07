a2fedd267c4d9fc514dce786d61f3f42d8e626c3
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2020-02-06 21:25:58 -0600

    Convert components to hooks (easier to test)

--- src/CeleryArchitectureDemo/ClientApp/src/features/counter/Counter.js
+++ src/CeleryArchitectureDemo/ClientApp/src/features/counter/Counter.js
@@ -1,32 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class Counter extends Component {
-  static displayName = Counter.name;
-
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
-
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Counter </h1>
-        <p> This is a simple example of a React component. </p>
-        <p>
-          Current count: <strong> {this.state.currentCount} </strong>
-        </p>
-        <button className="btn btn-primary" onClick={this.incrementCounter}>
-          Increment
-        </button>
-      </div>
-    );
-  }
+export function Counter() {
+  const [currentCount, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>This is a simple example of a React component.</p>
+      <p aria-live="polite">
+        Current count: <strong>{currentCount}</strong>
+      </p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setCount(currentCount + 1)}
+      >
+        Increment
+      </button>
+    </div>
+  );
 }
--- src/CeleryArchitectureDemo/ClientApp/src/features/weather/Weather.js
+++ src/CeleryArchitectureDemo/ClientApp/src/features/weather/Weather.js
@@ -1,56 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-export class Weather extends Component {
-  static displayName = Weather.name;
-
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-
-    fetch("api/Weather/Forecasts")
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ forecasts: data, loading: false });
-      });
-  }
-
-  static renderForecastsTable(forecasts) {
+export function Weather() {
+  const [forecasts, setForecasts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("api/Weather/Forecasts");
+      const data = await response.json();
+      setForecasts(data);
+      setLoading(false);
+    })();
+  }, []);
+  function renderForecastsTable(forecasts) {
     return (
-      <table className="table table-striped">
+      <table className="table table-striped" aria-labelledby="tableLabel">
         <thead>
           <tr>
-            <th> Date </th> <th> Temp.(C) </th> <th> Temp.(F) </th>
-            <th> Summary </th>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
           </tr>
         </thead>
         <tbody>
           {forecasts.map(forecast => (
-            <tr key={forecast.dateFormatted}>
-              <td>{forecast.dateFormatted} </td>
-              <td> {forecast.temperatureC} </td>
-              <td> {forecast.temperatureF} </td> <td> {forecast.summary} </td>
+            <tr key={forecast.date}>
+              <td>{forecast.dateFormatted}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
             </tr>
           ))}
         </tbody>
       </table>
     );
   }
-
-  render() {
-    let contents = this.state.loading ? (
-      <p>
-        <em> Loading...</em>
-      </p>
-    ) : (
-      Weather.renderForecastsTable(this.state.forecasts)
-    );
-
+  function renderLoadingMessage() {
     return (
-      <div>
-        <h1>Weather forecast </h1>
-        <p> This component demonstrates fetching data from the server. </p>
-        {contents}
-      </div>
+      <p>
+        <em>Loading...</em>
+      </p>
     );
   }
+  return (
+    <div>
+      <h1 id="tableLabel">Weather forecast</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      {loading ? renderLoadingMessage() : renderForecastsTable(forecasts)}
+    </div>
+  );
 }
