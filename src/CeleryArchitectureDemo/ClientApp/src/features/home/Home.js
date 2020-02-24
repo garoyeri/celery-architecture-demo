1ae1d087086d5da1ca92beb48eb26551e8e44678
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2020-02-24 12:45:27 -0600

    Fix some spelling / formatting

--- src/CeleryArchitectureDemo/ClientApp/src/features/home/Home.js
+++ src/CeleryArchitectureDemo/ClientApp/src/features/home/Home.js
@@ -10,19 +10,13 @@ export class Home extends Component {
         <p> Welcome to your new single - page application, built with:</p>
         <ul>
           <li>
-            <a href="https://get.asp.net/"> ASP.NET Core </a> and
-            <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-              C #
-            </a>
-            for cross - platform server - side code
+            <a href="https://get.asp.net/">ASP.NET Core</a> and <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">C#</a> for cross-platform server-side code
           </li>
           <li>
-            <a href="https://facebook.github.io/react/"> React </a>
-            for client - side code
+            <a href="https://facebook.github.io/react/">React</a> for client - side code
           </li>
           <li>
-            <a href="http://getbootstrap.com/"> Bootstrap </a>
-            for layout and styling
+            <a href="http://getbootstrap.com/">Bootstrap</a> for layout and styling
           </li>
         </ul>
         <p> To help you get started, we have also set up:</p>
